3e929c6fb8e143655ce81937be6ec397fb29f056
VladimirOnly <plokhotnuik@gmail.com>
2019-04-11 16:50:22 +0200

    finalJS

--- VPforCV.js
+++ VPforCV.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const ul = document.querySelector('ul');
+    let hash = location.hash;
     ul.innerHtml = ''; //clear list
-    const hash = location.hash;
     const tags = (hash.replace('#tags=', '')).split(',');
     tags.forEach((tag) => {
         const li = document.createElement('li');
@@ -10,4 +10,12 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
-//#tags=red,blue,purple
+
+// Once the button 'allTags' is clicked the text of the input element is added as a new tag into the URL hash (#tags=red,blue,purple,mytag)
+const button = document.getElementById('myButton');
+button.onclick = function() {
+    const links = document.querySelector('a');
+    const myTag = document.getElementById('textInput').value;
+    links.href += ',' + myTag;
+};
+
